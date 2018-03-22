c9c222020b62d0ed7c990b7a9e2f9c7c5d5a4aef
przemwo <przemwo@o2.pl>
2018-03-22 18:21:11 +0100

    video04 final

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 const express = require('express');
+const morgan = require('morgan');
+
 const app = express();
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
+// Middlewares
+app.use(morgan('dev'));
+
+// Routes
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500).json({
+        error: {
+            message: error.message
+        }
+    });
+});
 
 module.exports = app;
