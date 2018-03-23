18b845244d4e5d0ed4edcafc3533cc4e82310c32
przemwo <przemwo@o2.pl>
2018-03-23 08:23:59 +0100

    video05 final

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const app = express();
 
@@ -8,6 +9,31 @@ const orderRoutes = require('./api/routes/orders');
 
 // Middlewares
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Handling CORS
+app.use((req, res, next) => {
+    // Specify who has access to your api
+    res.header('Access-Control-Allow-Origin', '*');
+    // Specify other allowed headers for incoming request
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    // If this is an OPTIONS request...
+    if(req.method === 'OPTIONS') {
+        // ...specify allowed methods for incoming request...
+        res.header(
+            'Access-Control-Allow-Methods',
+            'PUT, POST, PATCH, DELETE, GET'
+        );
+        // ...and send '200' response
+        return res.status(200).json({});
+    }
+    // If this is not an OPTIONS request, don't return anything and go to the next middleware
+    next();
+});
 
 // Routes
 app.use('/products', productRoutes);
